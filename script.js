1b80f15bb6b68f83ccfab3b2354a2ad7fe4d4949
IdindlaPavankumar <idindlapavan@gmail.com>
2024-10-17 18:47:43 +0530

    day4 PS

---
+++ script.js
@@ -0,0 +1,42 @@
+let monthname = +prompt('enter the month number 0 to 11')
+switch (monthname) {
+    case 0:
+        console.log('january')
+        break;
+    case 1:
+        console.log('febrarary')
+        break;
+    case 2:
+        console.log('march')
+        break;
+    case 3:
+        console.log('april')
+        break;
+    case 4:
+        console.log('may')
+        break;
+    case 5:
+        console.log('june')
+        break;
+    case 6:
+        console.log('july')
+        break;
+    case 7:
+        console.log('august')
+        break;
+    case 8:
+        console.log('september')
+        break;
+    case 9:
+        console.log('Octomber')
+        break;
+    case 10:
+        console.log('November')
+        break;
+    case 11:
+        console.log('December')
+        break;
+    default:
+        console.log('you did mistake once the month number')
+
+}
